b8cbc64a44867327b335f7747d663052deb9778c
Kamil Otyński <Kamcio99@interia.com>
2022-03-28 21:06:04 +0200

    minor changes in new client and company validation

--- src/main/java/pl/saqie/InvoiceApp/app/components/client/usecase/register/dto/RegisterClientDto.java
+++ src/main/java/pl/saqie/InvoiceApp/app/components/client/usecase/register/dto/RegisterClientDto.java
@@ -3,6 +3,7 @@ package pl.saqie.InvoiceApp.app.components.client.usecase.register.dto;
 import lombok.*;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
@@ -14,13 +15,16 @@ import javax.validation.constraints.Size;
 public class RegisterClientDto {
 
     @Size(min = 3, max = 15, message = "Nazwa musi skladac sie od 3 do 15 znakow.")
+    @NotBlank(message = "Pole nie moze zawierac bialych znakow")
     private String username;
     @Email(message = "Wprowadz poprawny adres email")
     @NotEmpty(message = "Email nie moze pozostac pusty.")
     private String email;
     @Size(min = 5, max = 20, message = "Haslo musi zawierac od 5 do 20 znakow.")
+    @NotBlank(message = "Haslo nie moze zawierac bialych znakow")
     private String password;
     @NotEmpty(message = "To pole nie może pozostać puste.")
+    @NotBlank(message = "Haslo nie moze zawierac bialych znakow")
     private String passwordRepeat;
 
 }
--- src/main/java/pl/saqie/InvoiceApp/app/components/company/usecase/newcompany/NewCompanyDto.java
+++ src/main/java/pl/saqie/InvoiceApp/app/components/company/usecase/newcompany/NewCompanyDto.java
@@ -18,9 +18,10 @@ public class NewCompanyDto {
     @Size(min = 3,message = "Nazwa firmy musi zawierac co najmniej 3 znaki")
     private String name;
     @NotEmpty(message = "Pole nie moze pozostac puste")
-    @NotBlank(message = "Pole nie moze pozostac puste")
+    @NotBlank(message = "Pole nie moze zawierac bialych znakow")
     private String adress;
     @Size(min = 9, max = 9, message = "Podaj poprawny numer telefonu")
+    @NotBlank(message = "Pole nie moze zawierac bialych znakow")
     private String telephoneNumber;
     @NIP(message = "Wprowadz poprawny numer NIP")
     private String nip;
