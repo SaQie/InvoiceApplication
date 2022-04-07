8f20182d984185ec326ad6d8ec9cad7b4e8ffa0b
Kamil Otyński <Kamcio99@interia.com>
2022-04-07 20:00:10 +0200

    auth integration tests

--- src/test/java/pl/saqie/InvoiceApp/integration/ClientAuthUseCaseTest.java
+++ src/test/java/pl/saqie/InvoiceApp/integration/ClientAuthUseCaseTest.java
@@ -4,10 +4,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import pl.saqie.InvoiceApp.app.components.client.entity.Client;
 import pl.saqie.InvoiceApp.app.components.client.entity.Role;
 import pl.saqie.InvoiceApp.app.components.client.usecase.ClientAuthUseCase;
@@ -18,19 +24,23 @@ import pl.saqie.InvoiceApp.app.components.client.usecase.register.validator.exce
 import javax.sql.DataSource;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ClientAuthUseCaseTest {
 
     private final DataSource dataSource;
     private final JdbcTemplate jdbcTemplate;
     private final ClientAuthUseCase authUseCase;
+    private final MockMvc mockMvc;
 
     @Autowired
-    ClientAuthUseCaseTest(DataSource dataSource, JdbcTemplate jdbcTemplate, ClientAuthUseCase authUseCase) {
+    ClientAuthUseCaseTest(DataSource dataSource, JdbcTemplate jdbcTemplate, ClientAuthUseCase authUseCase, MockMvc mockMvc) {
         this.dataSource = dataSource;
         this.jdbcTemplate = jdbcTemplate;
         this.authUseCase = authUseCase;
+        this.mockMvc = mockMvc;
     }
 
     @BeforeEach
@@ -113,6 +123,32 @@ class ClientAuthUseCaseTest {
         // then
         assertThrows(ClientExistsException.class,
                 () -> authUseCase.registerClient(registerClientDto));
+    }
+
+    @Test
+    @WithUserDetails(value = "username")
+    void shouldReturnStatusOf200ForTheLoggedInClient() throws Exception {
+        // given
+
+        // when
+
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.get("/add/company"))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    @WithAnonymousUser
+    void shouldReturnStatusOf400ForTheAnonymousClient() throws Exception {
+        // given
+
+        // when
+
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.get("/add/company"))
+                .andExpect(status().isUnauthorized());
+
     }
 
 }
